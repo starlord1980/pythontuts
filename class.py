f5148ff5e5a9b77fbb6d5ece845ce7c0803057a0
James <jfanthorpe@mac.com>
2017-12-28 12:47:38 +0000

    added a new file

---
+++ class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class ClassName:
+	pass
+
+instance = ClassName()
+class Students:
+		def __init__(emp, name, age, pos):
+			emp.name = name
+			emp.age = age
+			emp.pos = pos
+		def displayEmployee(self):
+			return("Employee name is " + emp1.name + " who is " + str(emp1.age) + " and is a " + emp1.pos)
+
+
+emp1 = Students("Bob", 12, "Carpenter")
+emp2 = Students("Bella", 22, "Bobsledder")
+print (emp1.name)
+print (emp1.age)
+print (emp1.pos)
+
+print(emp1.displayEmployee())
